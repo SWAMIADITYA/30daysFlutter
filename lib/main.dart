b11f1a0c5f76b02499acbfe7becc78eae3f554fc
Aditya Swami <adityaswami098@gmail.com>
2023-10-13 19:17:26 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:lession1/pages/home_page.dart';
 import 'package:lession1/pages/login_page.dart';
 
@@ -11,6 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       //home: LoginPage(),
+      theme: ThemeData(
+        fontFamily: GoogleFonts.prompt().fontFamily,
+      ),
       initialRoute: "/login",
       routes: {
         "/" : (context)=> HomePage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,11 +6,46 @@ class LoginPage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Material(
-      child: Center(child: Text("LOGIN PAGE" , style: TextStyle(
-        fontSize: 40,
-        color: Colors.amber,
-        fontWeight: FontWeight.bold, 
-      ),),),
+      color: Colors.white,
+    child: Column(
+      children: [
+        Image.asset("assets/images/login_image.png", fit: BoxFit.cover,),
+        SizedBox(height:20.0),
+        Text("WELCOME", style: TextStyle(
+          fontSize: 25,
+          color: Colors.red,
+          fontWeight: FontWeight.bold,
+        ),),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 32.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Enter username",
+                  labelText: "Username",
+                ),
+              ),
+        
+               TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: "Enter password",
+              labelText: "Password",
+            ),
+          ),
+          SizedBox(height: 30.0,),
+
+          ElevatedButton(onPressed: (){
+            print("hii aditya");
+          }, child: Text("Login")),
+            ],
+          ),
+        ),
+       
+
+      ],
+    ),
     );
   }
 }
